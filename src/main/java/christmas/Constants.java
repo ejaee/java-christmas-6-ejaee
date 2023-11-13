33ad18e6061a6416a903b3de1696e484645f9e72
choiejae <ejae8319@gmail.com>
2023-11-13 23:06:51 +0900

    feat(InputView): 콘솔 기반 입력 기능 구현
    - 고객 방문 날짜 입력 기능
    - 주문 입력 기능

---
+++ src/main/java/christmas/Constants.java
@@ -0,0 +1,6 @@
+package christmas;
+
+public class Constants {
+    public static final int YEAR = 2023;
+    public static final int MONTH = 12;
+}
---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,24 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.Constants;
+
+public class InputView {
+
+    public static final String VISIT_DATE_REQUEST_MESSAGE = "%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String ORDER_REQUEST_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
+    public String inputVisitDate() {
+        System.out.printf((VISIT_DATE_REQUEST_MESSAGE) + "\n", Constants.MONTH);
+        return input();
+    }
+
+    public String inputOrder() {
+        System.out.println(ORDER_REQUEST_MESSAGE);
+        return input();
+    }
+
+    private String input() {
+        return Console.readLine();
+    }
+}
