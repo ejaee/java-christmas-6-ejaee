cf03f0984241d9fe740fed4e7b646f358feaad26
choiejae <ejae8319@gmail.com>
2023-11-14 18:40:44 +0900

    feat(OrderService): Order 를 처리하는 서비스 기능 구현
    - 음료만 주문할 경우 예외를 발생한다
    - 주문 메뉴 총 수량이 허용치를 초과하면 예외를 발생한다
    - OrderTest: Order 내에서 총 주문 금액 계산 테스트

---
+++ src/main/java/christmas/service/OrderService.java
@@ -0,0 +1,67 @@
+package christmas.service;
+
+import christmas.domain.order.MenuCount;
+import christmas.domain.order.MenuName;
+import christmas.domain.order.Order;
+import christmas.utils.parser.KeyValueParser;
+import christmas.view.ErrorMessage;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class OrderService {
+
+    private static final String BEVERAGE_CATEGORY = "음료";
+    public static final int MEX_ORDER_COUNT = 20;
+
+    public Order generateOrder(final String inputValue) {
+        Map<String, Long> menuInfo = KeyValueParser.parseStringToMap(inputValue);
+        Map<MenuName, MenuCount> order = putMenuInfo(menuInfo);
+
+        validate(order);
+        return new Order(order);
+    }
+
+    public long getOrderCost(final Order order) {
+        return order.getTotalOrderCost();
+    }
+
+    private static Map<MenuName, MenuCount> putMenuInfo(Map<String, Long> menuInfo) {
+        Map<MenuName, MenuCount> order = new LinkedHashMap<>();
+
+        menuInfo.forEach((menuItemName, count) -> {
+            MenuName menuName = MenuName.findByName(menuItemName);
+            MenuCount menuCount = MenuCount.of(count);
+            order.put(menuName, menuCount);
+        });
+        return order;
+    }
+
+    private void validate(Map<MenuName, MenuCount> order) {
+        validateMenuCategory(order);
+        validateMenuTotalCount(order);
+    }
+
+    private void validateMenuCategory(Map<MenuName, MenuCount> order) {
+        if (isOnlyBeverages(order)) {
+            throw new IllegalArgumentException(ErrorMessage.MENU_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    private boolean isOnlyBeverages(Map<MenuName, MenuCount> order) {
+        return order.keySet().stream()
+                .allMatch(item -> item.getCategory().equals(BEVERAGE_CATEGORY));
+    }
+
+    private void validateMenuTotalCount(Map<MenuName, MenuCount> order) {
+        long totalMenuCount = getTotalMenuCount(order);
+        if (totalMenuCount > MEX_ORDER_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.MENU_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    private long getTotalMenuCount(Map<MenuName, MenuCount> order) {
+        return order.values().stream()
+                .mapToLong(MenuCount::count)
+                .sum();
+    }
+}
---
+++ src/test/java/christmas/service/OrderServiceTest.java
@@ -0,0 +1,74 @@
+package christmas.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import christmas.domain.order.Order;
+import christmas.view.ErrorMessage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceTest {
+
+    private OrderService orderService;
+
+    @BeforeEach
+    void setUp() {
+        orderService = new OrderService();
+    }
+
+    @DisplayName("orderService 를 통해 사용자 입력 값으로 Order 를 생성한다.")
+    @Test
+    void generateOrder() {
+        // given
+        String inputValue = "제로콜라-1,초코케이크-2";
+
+        // when
+        Order order = orderService.generateOrder(inputValue);
+
+        // then
+        assertThat(order).isNotNull();
+    }
+
+    @DisplayName("orderService 를 통해 order 의 총 주문 금액을 반환한다.")
+    @Test
+    void getOrderCost() {
+        // given
+        String inputValue = "제로콜라-1,초코케이크-2";
+        Order order = orderService.generateOrder(inputValue);
+
+        // when
+        long orderCost = orderService.getOrderCost(order);
+
+        // then
+        Assertions.assertEquals(33_000, orderCost);
+    }
+
+    @DisplayName("음료만 주문하는 경우 예외를 발생한다.")
+    @Test
+    void throwExceptionIfMenuAllBeverages() {
+        // given
+        String inputValue = "제로콜라-1,레드와인-3";
+
+        // when
+        // then
+        assertThatThrownBy(() -> orderService.generateOrder(inputValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.MENU_ERROR_MESSAGE.getMessage());
+    }
+
+    @DisplayName("주문 메뉴 총 수량이 허용치를 초과하면 예외를 발생한다.")
+    @Test
+    void throwExceptionIfOrderCountExceeded() {
+        // given
+        String inputValue = "타파스-11,초코케이크-30";
+
+        // when
+        // then
+        assertThatThrownBy(() -> orderService.generateOrder(inputValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.MENU_ERROR_MESSAGE.getMessage());
+    }
+}
