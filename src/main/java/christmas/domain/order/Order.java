27d1f70a5957c5b17c9b0a73ccd202a6afece065
choiejae <ejae8319@gmail.com>
2023-11-14 17:42:01 +0900

    feat(Order): 고객의 총 주문 정보를 저장하는 기능 구현
    - getTotalOrderCost(): 주문에 맞는 지불 비용을 반환한다

---
+++ src/main/java/christmas/domain/order/Order.java
@@ -0,0 +1,22 @@
+package christmas.domain.order;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public record Order(Map<MenuName, MenuCount> order) {
+
+    private static final String ORDER_MENU_TEMPLATE = "%s %s개";
+
+    public long getTotalOrderCost() {
+        return order.entrySet().stream()
+                .mapToLong(entry -> entry.getKey().getPrice() * entry.getValue().count())
+                .sum();
+    }
+
+    @Override
+    public String toString() {
+        return order.entrySet().stream()
+                .map(entry -> String.format(ORDER_MENU_TEMPLATE, entry.getKey(), entry.getValue()))
+                .collect(Collectors.joining("\n"));
+    }
+}
