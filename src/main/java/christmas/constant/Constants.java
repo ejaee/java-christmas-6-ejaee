eb9f5c90426f9c6df27c5d7c11db377232474462
choiejae <ejae8319@gmail.com>
2023-11-15 01:06:20 +0900

    feat(Event): 이벤트플레너에서 다루는 Event 를 저장하는 기능 구현
    - 크리스마스 디데이 할인
    - 평일 / 주말 할인
    - 특별 할인

--- src/main/java/christmas/constant/Constants.java
+++ src/main/java/christmas/constant/Constants.java
@@ -3,4 +3,11 @@ package christmas.constant;
 public class Constants {
     public static final int YEAR = 2023;
     public static final int MONTH = 12;
+
+    public static final long GIFT_EVENT_BASE_PRICE = 120_000L;
+    public static final long EVENT_MINIMUM_CONDITION_PRICE = 10_000L;
+
+    public static final String GIFT_EVENT_NAME = "증정 이벤트";
+    public static final int LAST_DAY_OF_CHRISTMAS_EVENT = 25;
+    public static final int BASE_DISCOUNT_AMOUNT_OF_CHRISTMAS_EVENT = -100;
 }
---
+++ src/main/java/christmas/constant/Event.java
@@ -0,0 +1,56 @@
+package christmas.constant;
+
+import christmas.domain.order.VisitDate;
+import christmas.utils.DateUtils;
+
+public enum Event {
+
+    CHRISTMAS("크리스마스 디데이 할인", -1_000, "100"),
+    WEEKDAY("평일 할인", -2_023, "디저트"),
+    WEEKEND("주말 할인", -2_023, "메인"),
+    SPECIAL("특별 할인", -1_000, "별"),
+    NONE("없음", 0, "없음");
+
+    private final String eventName;
+    private final long discountStandardPrice;
+    private final String standardKeyWord;
+
+    Event(String eventName, long discountStandardPrice, String standardKeyWord) {
+        this.eventName = eventName;
+        this.discountStandardPrice = discountStandardPrice;
+        this.standardKeyWord = standardKeyWord;
+    }
+
+    public static Event getChristmasEvent(final VisitDate visitDate) {
+        if (visitDate.getDayOfMonth() > Constants.LAST_DAY_OF_CHRISTMAS_EVENT) {
+            return NONE;
+        }
+        return CHRISTMAS;
+    }
+
+    public static Event getDayOfWeekEvent(final VisitDate visitDate) {
+        if (DateUtils.isWeekend(visitDate.getDayOfWeek())) {
+            return WEEKEND;
+        }
+        return WEEKDAY;
+    }
+
+    public static Event getSpecialEvent(final VisitDate visitDate) {
+        if (EventCalendar.isPossibleSpecialEvent(visitDate, SPECIAL.getStandardKeyWord())) {
+            return SPECIAL;
+        }
+        return NONE;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public long getDiscountStandardPrice() {
+        return discountStandardPrice;
+    }
+
+    public String getStandardKeyWord() {
+        return standardKeyWord;
+    }
+}
